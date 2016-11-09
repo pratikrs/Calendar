bb8761dc83ca3ae4814611c128e21b1875d68c63
pratikrs <pratik@rachaitasolutions.com>
2016-11-10 01:28:24 +0530

    Add files via upload

---
+++ CalendarModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomCalendar.Models
+{
+    public class CalendarModel
+    {
+    }
+
+    public class WeekForMonth
+    {
+        public List<Day> Week1 { get; set; }
+        public List<Day> Week2 { get; set; }
+        public List<Day> Week3 { get; set; }
+        public List<Day> Week4 { get; set; }
+        public List<Day> Week5 { get; set; }
+        public List<Day> Week6 { get; set; }
+        public string nextMonth { get; set; }
+        public string prevMonth { get; set; }
+    }
+
+    public class Day
+    {
+        public DateTime Date { get; set; }
+        public string _Date { get; set; }
+        public string dateStr { get; set; }
+        public int dtDay { get; set; }
+        public int? daycolumn { get; set; }
+
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,47 @@
+﻿using CustomCalendar.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomCalendar.Controllers
+{
+    public class HomeController : Controller
+    {
+        private CalendarManager _calendar = new CalendarManager();
+
+        public ActionResult Index()
+        {
+            var model = _calendar.getCalender(DateTime.Now.Month, DateTime.Now.Year);
+            return View(model);
+        }
+
+        public ActionResult AsyncUpdateCalender(int month, int year)
+        {
+            if (HttpContext.Request.IsAjaxRequest())
+            {
+                var model = _calendar.getCalender(month, year);
+                return Json(model, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
